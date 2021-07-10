aacecb32238c75872878253b06269240f23d86a5
Kristaps Grinbergs <fassko@gmail.com>
2021-07-10 16:11:13 +0300

    Initial Commit

---
+++ SwiftUILocalization/SwiftUILocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILocalizationApp.swift
+//  SwiftUILocalization
+//
+//  Created by Kristaps Grinbergs on 10/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
